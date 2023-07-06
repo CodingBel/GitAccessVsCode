8de46575dc6298b1889645ff6aa8925914ce5e00
Abel <belaashu23@gmail.com>
2023-07-06 12:23:47 +0200

    First Commit From VS Code

---
+++ Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std; 
+
+int main () {
+	cout << "Hello Bela in Torino, Italy" << endl; 
+	cout << "This challenging but Fun" << endl; 
+	cout << "Lets upload this from VS Code" << endl; 
+	
+	return 0; 
+}
